2fe3aa548ab092d9582985b3b95007833df04eab
Low-Key JC <79029972+jcahsy170324@users.noreply.github.com>
2022-10-02 21:42:42 +0800

    热点规则

--- cloudalibaba-sentinel-service8401/src/main/java/com/example/cloudalibabasentinelservice8401/controller/TestController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/example/cloudalibabasentinelservice8401/controller/TestController.java
@@ -1,8 +1,11 @@
 package com.example.cloudalibabasentinelservice8401.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.example.cloudalibabasentinelservice8401.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
@@ -37,5 +40,31 @@ public class TestController {
     @RequestMapping("/testService")
     public String testService(){
         return testService.common();
+    }
+
+    @RequestMapping("/testC")
+    public String testC(){
+        return "testC";
+    }
+
+    @RequestMapping("/testD")
+    public String testD(Integer id){
+        if (id != null && id > 1){
+            throw new RuntimeException("异常熔断");
+        }
+        return "testD";
+    }
+
+    @RequestMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler = "myBlockHandler")
+    public String testHotKey(@RequestParam(value = "hot1",required = false)String hot1,
+                             @RequestParam(value = "hot2",required = false)String hot2,
+                             @RequestParam(value = "hot3",required = false)String hot3){
+
+        return "testHotKey...";
+    }
+
+    public String myBlockHandler(String hot1, String hot2, String hot3, BlockException exception){
+        return "System is busy";
     }
 }
